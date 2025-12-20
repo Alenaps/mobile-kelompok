88ef1a1f7487b092e20eb1f611089176f8d98b0c
Alenaps <131380975+Alenaps@users.noreply.github.com>
2025-12-20 15:53:52 +0700

    integrasi login-register ke firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'routes.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
@@ -18,7 +26,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       initialRoute: '/',
-      routes: appRoutes, // ✅ sekarang dikenali
+      routes: appRoutes,
     );
   }
 }
